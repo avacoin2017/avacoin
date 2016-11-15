98673c6267cf9fb31ff8ba804c1b3f4af4cfad28
builders1965 <ivanov.ivan2655@yandex.ru>
2016-11-15 16:38:58 +0600

    15.11

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define AvaCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "AvaCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
